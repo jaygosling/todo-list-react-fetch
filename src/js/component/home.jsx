2eae1fa45d846d9e047871ff47178bb45fd241eb
jaygosling <javijimenezclemente@gmail.com>
2022-04-25 19:51:38 +0000

    Versión Beta

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,66 @@
-import React from "react";
-
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import React, { useState, useEffect } from "react";
 
 //create your first component
 const Home = () => {
+	const [tasks, addTask] = useState([]);
+	const [numTask, addNumTask] = useState(0);
 	return (
-		<div>
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="bg-light h-100">
+			<h1 className="display-1 text-secondary text-center">todos</h1>
+			<ul className="list-group w-50 mx-auto mt-5 pb-5">
+				<li className="list-group-item">
+					<input
+						className="form-control border-0 p-0 insertTask"
+						type="text"
+						placeholder="Add new item..."
+						aria-label="taskToAdd"
+						onKeyPress={(e) => {
+							if (e.key == "Enter") {
+								addTask([
+									...tasks,
+									document.querySelector(".insertTask").value,
+								]);
+								document.querySelector(".insertTask").value =
+									"";
+								addNumTask(numTask + 1);
+							}
+						}}
+					/>
+				</li>
+				{tasks.map((value, index) => {
+					if (value != null) {
+						return (
+							<li className="list-group-item" key={`${index}`}>
+								<div className="d-flex">
+									<div className="col-10">{value}</div>
+									<div className="col-2 text-end">
+										<i
+											className="fas fa-times"
+											id="cross"
+											onClick={() => {
+												addNumTask(numTask - 1);
+												let newItemsArray = [];
+												tasks.forEach((v, i) => {
+													if (i == index) {
+														newItemsArray.push(
+															null
+														);
+													} else {
+														newItemsArray.push(v);
+													}
+												});
+												addTask(newItemsArray);
+											}}></i>
+									</div>
+								</div>
+							</li>
+						);
+					}
+				})}
+				<li className="list-group-item text-muted fs-6">
+					{numTask} items left
+				</li>
+			</ul>
 		</div>
 	);
 };
