cf4daffc41d908dbb6031c6cbaa62f54d56e466a
jaygosling <javijimenezclemente@gmail.com>
2022-04-28 11:11:02 +0000

    Converting original react todo list into api assisted list

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,8 +1,50 @@
 import React, { useState, useEffect } from "react";
 
 //create your first component
+
 const Home = () => {
 	const [tasks, addTask] = useState([]);
+	const getList = (addTask) => {
+		var requestOptions = {
+			method: "GET",
+			redirect: "follow",
+		};
+
+		fetch(
+			"https://assets.breatheco.de/apis/fake/todos/user/jaygosling",
+			requestOptions
+		)
+			.then((response) => response.json())
+			.then((result) => addTask(result))
+			.catch((error) => console.log("error", error));
+	};
+	const sendList = (updList) => {
+		var myHeaders = new Headers();
+		myHeaders.append("Content-Type", "application/json");
+
+		var raw = JSON.stringify(updList);
+
+		var requestOptions = {
+			method: "PUT",
+			headers: myHeaders,
+			body: raw,
+			redirect: "follow",
+		};
+
+		fetch(
+			"https://assets.breatheco.de/apis/fake/todos/user/jaygosling",
+			requestOptions
+		)
+			.then((response) => response.text())
+			.then((result) => console.log(result))
+			.catch((error) => console.log("error", error));
+	};
+	useEffect(() => {
+		getList(addTask);
+	}, []);
+	useEffect(() => {
+		sendList(tasks);
+	}, [tasks]);
 	return (
 		<div className="bg-light h-100">
 			<h1 className="display-1 text-secondary text-center">todos</h1>
@@ -15,37 +57,52 @@ const Home = () => {
 						aria-label="taskToAdd"
 						onKeyPress={(e) => {
 							if (e.key == "Enter") {
-								addTask([...tasks, e.target.value]);
+								addTask([
+									...tasks,
+									{ label: e.target.value, done: false },
+								]);
 								e.target.value = "";
 							}
 						}}
 					/>
 				</li>
-				{tasks.map((value, index) => {
-					return (
-						<li className="list-group-item" key={`${index}`}>
-							<div className="d-flex">
-								<div className="col-10">{value}</div>
-								<div className="col-2 text-end">
-									<i
-										className="fas fa-times"
-										id="cross"
-										onClick={() => {
-											let newItemsArray = tasks.filter(
-												(v, i) => {
-													return i != index;
-												}
-											);
-											addTask(newItemsArray);
-										}}></i>
+				{tasks.map((object, index) => {
+					if (object.done == false) {
+						return (
+							<li className="list-group-item" key={`${index}`}>
+								<div className="d-flex">
+									<div className="col-10">{object.label}</div>
+									<div className="col-2 text-end">
+										<i
+											className="fas fa-times"
+											id="cross"
+											onClick={() => {
+												let newItemsArray =
+													tasks.filter((v, i) => {
+														return i != index;
+													});
+												addTask(newItemsArray);
+											}}></i>
+									</div>
 								</div>
-							</div>
-						</li>
-					);
+							</li>
+						);
+					}
 				})}
 
-				<li className="list-group-item text-muted fs-6">
-					{tasks.length} items left
+				<li className="list-group-item text-center">
+					<button
+						className="btn btn-outline-dark btn-sm"
+						onClick={() => {
+							addTask([
+								{
+									label: "Always one task remaining",
+									done: true,
+								},
+							]);
+						}}>
+						Clear all
+					</button>
 				</li>
 			</ul>
 		</div>
